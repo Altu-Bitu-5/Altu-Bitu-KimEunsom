454f8b07528fdb2f3b1338637e75dc08d44164c5
unknown <somi5488@naver.com>
2023-11-08 18:48:31 +0900

    [이분탐색] 11월 8일

---
+++ 10_이분탐색/3079.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//이분 탐색
+unsigned long long binarySearch(const vector<unsigned long long>& t, unsigned long long m, unsigned long long left, unsigned long long right) {
+    unsigned long long res = 0;
+    unsigned long long mid = 0;
+    unsigned long long q = 0;
+    
+    while (left <= right) {
+        mid = (left + right) / 2;
+        q=0;
+        
+        for (int t : t) {
+            q += mid / t;
+        }
+        
+        if (q >= m) { //m명 다 심사함
+            right = mid - 1;
+            res = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    return res;
+}
+
+int main() {
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    unsigned long long n,m,temp; //자료형 주의! 아니면 시간 초과..
+    
+    
+    vector <unsigned long long> t;
+    
+    //입력
+    cin >> n >> m;
+    while(n--) {
+        cin >> temp;
+        t.push_back(temp);
+    }
+    
+    //이분탐색 전 정렬 필수 !
+    sort(t.begin(), t.end());
+    
+    cout << binarySearch(t, m, 0, t[0] * m);
+    
+    return 0;
+}
